07df89bbea578a31fd05b99f7a4132ec6605fed6
Morgan Nyman <melon21@hotmail.se>
2025-12-22 18:34:47 +0100

    Update runme.sh to Detech the Linux Distro (hopefully
    Add distro variable and download VSCodium settings

--- runme.sh
+++ runme.sh
@@ -1,3 +1,5 @@
 !#usr/bin/env bash
 
+distro="$(awk -F= '/^NAME/{print $2}' /etc/os-release)"
+
 curl --output ~/.config/VSCodium/User/settings.json "https://raw.githubusercontent.com/mogggen/dotfiles/refs/heads/main/VSCodium/User/settings.json"
